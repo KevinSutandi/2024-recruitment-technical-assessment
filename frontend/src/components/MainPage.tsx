35a38e8e30ac2826a784b1a1bf4b4cd95ecb7714
Kevin Sutandi <kevins050324@gmail.com>
2024-02-21 16:28:49 +1100

    Stretch task 1 done

--- frontend/src/components/MainPage.tsx
+++ frontend/src/components/MainPage.tsx
@@ -1,29 +1,48 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import SearchBar from './MainPageComponents/SearchBar';
 import Dropdown from './MainPageComponents/Dropdown';
 import CourseCard from './MainPageComponents/CourseCard';
 import courses from '../../courses.json';
 
 export default function MainPage() {
+  const [isUnilectivesClicked, setIsUnilectivesClicked] = useState(false);
+
+  const handleUnilectivesClick = () => {
+    setIsUnilectivesClicked(!isUnilectivesClicked);
+  };
+
   return (
     <div className='ml-20 w-full'>
       <div className='flex flex-col mx-32 my-5 gap-4'>
         <p className='text-sm'>DevSoc Presents</p>
-        <p className='text-blue-500 text-7xl font-bold'>unilectives</p>
+        <p
+          className={`text-blue-500 text-7xl font-bold cursor-pointer ${
+            isUnilectivesClicked ? 'text-blue-100' : ''
+          }`}
+          onClick={handleUnilectivesClick}
+        >
+          unilectives
+        </p>
         <p className='font-bold'>
           Your one-stop shop for UNSW course and elective reviews.
         </p>
         <SearchBar />
         <Dropdown />
         {/* CourseCard will be used in a for loop to get it from json */}
-        <CourseCard
-          coursePrefix='COMP'
-          courseCode={1511}
-          courseTitle='Programming Fundamentals'
-          averageStars={4.8}
-          totalReviews={64}
-          offeredTerms={['T1', 'T2']}
-        />
+        <div className='grid grid-cols-3 gap-6'>
+          {courses.map((course, index) => (
+            <CourseCard
+              key={index}
+              coursePrefix={course.course_prefix}
+              courseCode={course.course_code}
+              courseTitle={course.course_title}
+              averageStars={course.average_stars}
+              totalReviews={course.total_reviews}
+              offeredTerms={course.offered_terms}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/MainPageComponents/CourseCard.tsx
+++ frontend/src/components/MainPageComponents/CourseCard.tsx
@@ -40,6 +40,23 @@ export default function CourseCard({
     return starIcons;
   };
 
+  const generateTermBubbles = (offeredTerms: string[]): JSX.Element[] => {
+    const termBubbles: JSX.Element[] = [];
+
+    for (let i = 0; i < offeredTerms.length; i++) {
+      termBubbles.push(
+        <div
+          key={i}
+          className='bg-blue-100 text-black text-xs px-2 py-1 rounded-xl'
+        >
+          {offeredTerms[i]}
+        </div>
+      );
+    }
+
+    return termBubbles;
+  };
+
   return (
     <div className='flex flex-col px-6 w-80 h-48 bg-white rounded-lg shadow-lg'>
       <div className='flex flex-row py-4 items-center justify-between'>
@@ -53,14 +70,12 @@ export default function CourseCard({
           <p className='text-xs text-gray-400'>{totalReviews} Reviews</p>
         </div>
       </div>
-      <div className='flex flex-row justify-between'>
+      <div className='flex flex-row justify-between mb-14'>
         <p className='text-xs'>{courseTitle}</p>
       </div>
 
-      <div className='flex flex-row justify-between'>
-        <p className='text-xs text-gray-400'>
-          Offered in: {offeredTerms.join(', ')}
-        </p>
+      <div className='flex flex-row gap-3'>
+        {generateTermBubbles(offeredTerms)}
       </div>
     </div>
   );
